bc0d2afaf0acddeccd514dc23974172b6763cf00
Taylor Allen <taylorallen0913@gmail.com>
2020-05-30 14:15:36 -0700

    comment server code

--- python-server/server.py
+++ python-server/server.py
@@ -24,48 +24,57 @@ def root():
 
 @app.route('/transcribe/<string:video_id>', methods=['GET', 'POST'])
 def transcribe(video_id):
+
+    # Get youtube video from id and download it
     videoFile = YouTube(
         f'http://youtube.com/watch?v={video_id}').streams.filter(only_audio=True).first().download(filename="Video")
 
     print('Finished downloading video')
 
     storageCli = storage.Client()
-    # # get bucket
+    # Get GCP bucket and upload video
     bucket = storageCli.get_bucket('descriptor')
     blob = bucket.blob('Video.mp4')
     blob.upload_from_filename('Video.mp4')
 
     print('Finished uploading video')
 
+    # Set up GCP video intelligence service
     video_client = videointelligence.VideoIntelligenceServiceClient()
     features = [videointelligence.enums.Feature.SPEECH_TRANSCRIPTION]
 
     config = videointelligence.types.SpeechTranscriptionConfig(
-        language_code="en-US", enable_automatic_punctuation=True
+        language_code='en-US', enable_automatic_punctuation=True
     )
     video_context = videointelligence.types.VideoContext(
         speech_transcription_config=config
     )
+
     operation = video_client.annotate_video(
-        "gs://descriptor/Video.mp4", features=features, video_context=video_context
+        'gs://descriptor/Video.mp4', features=features, video_context=video_context
     )
 
+    # Get transcription of video
     result = operation.result(timeout=3000)
 
-    # There is only one annotation_result since only
-    # one video is processed.
+    # Format result of transcription call
     annotation_results = result.annotation_results[0]
-    wallOfText = ""
+    paragraph = ''
     for speech_transcription in annotation_results.speech_transcriptions:
         for alternative in speech_transcription.alternatives:
-            wallOfText += alternative.transcript
+            paragraph += alternative.transcript
 
-    paragraph = wallOfText
-    print('Finished transcribing video\n\n')
-    print(paragraph)
-
+    # Generate summary from input
     summary = generate_summary(paragraph)
-    print(summary)
+
+    # Format summary into notes form
     notes = format_summary(summary)
-    print(notes)
-    return "OK"
+
+    # What to return back in api call
+    data = {
+        'paragraph': paragraph,
+        'summary': summary,
+        'notes': notes
+    }
+
+    return jsonify(data)
